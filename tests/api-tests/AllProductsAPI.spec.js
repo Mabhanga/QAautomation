a29817c60d6dbe7c092ebbc73b869157780b3bc1
Mabanga <jaymotsweni@gmail.com>
2026-04-29 20:22:43 +0200

    Add API tests

---
+++ tests/api-tests/AllProductsAPI.spec.js
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('Get All Products', async ({ request }) => {
+    const response = await request.get('https://automationexercise.com/api/productsList');
+    
+    expect(response.status()).toBe(200);
+    
+    const body = await response.json();
+    expect(body).toHaveProperty('products');
+    expect(body.products.length).toBeGreaterThan(0);
+    expect(body.products[0]).toHaveProperty('name');
+    console.log(body);
+});
+
+
+test.only('POST To All Products - Method Not Allowed', async ({ request }) => {
+    const response = await request.post('https://automationexercise.com/api/productsList');
+    
+    expect(response.status()).toBe(200);
+    
+    const body = await response.json();
+    expect(body.responseCode).toBe(405);
+    expect(body.message).toBe('This request method is not supported.');
+});
